2abfceed7a6bfb6ec6d7e47e95c4736ab85ebd53
Thiago Smerdeck Pereira <59984081+Jaiketh@users.noreply.github.com>
2026-05-28 00:19:34 -0400

    Redact sensitive information in Sobre.vue

--- catalogo-bandas/src/pages/Sobre.vue
+++ catalogo-bandas/src/pages/Sobre.vue
@@ -12,8 +12,8 @@
     <v-card class="mt-4 pa-4">
       <v-card-title>Integrantes</v-card-title>
       <v-card-text>
-        <p><strong>Aluno 1:</strong> Thiago Smerdeck Pereira — Matrícula: 2019171452</p>
-        <p><strong>Aluno 2:</strong> Claudio Antropico da Silva — Matrícula: 2023031452</p>
+        <p><strong>Aluno 1:</strong> Thiago S***** Pereira — Matrícula: 0000000</p>
+        <p><strong>Aluno 2:</strong> Claudio Antropico da Silva — Matrícula: 202303</p>
       </v-card-text>
     </v-card>
   </v-container>
